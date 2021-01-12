bf5d615c3cf0a8b8d3fd32d94c3c62cb454f9a15
Alexander Portera <aportera3@gmail.com>
2021-01-12 10:04:37 -0500

    commit

---
+++ resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+
+    imagebaseurl: "https://ivy555.github.io/",
+    name: "Alexander Portera",
+    role: "Full Satck Developer",
+    linkedinId: "Your ID",    
+    aboutme: "",
+    roleDescription: "",
+    
+    socialLinks: [
+        {
+            name: "linkedin",
+            url: "https://www.linkedin.com/in/dilip-safi-438322a3/",
+            className: "fa fa-linkedin"
+        },
+        {
+            name: "github",
+            url: "http://github.com/ivy555",
+            className: "fa fa-github"
+        },
+    ],
+
+    education: [
+        {
+            UniversityName: "Rutgers",
+            specialization: "Full Stack Development",
+            MonthOfPassing: "Jan",
+            YearOfPassing: "2021",
+        },
+
+        {
+            UniversityName: "Kean University",
+            specialization: "BA Theater",
+            YearOfPassing: "2017",
+        }
+    ],
+
+    work: [
+        {
+            CompanyName: "",
+            specialization: "",
+            MonthOfLeaving: "",
+            YearOfLeaving: "",
+        },
+        {
+            CompanyName: "",
+            specialization: "",
+            MonthOfLeaving: "",
+            YearOfLeaving: "",
+        }
+    ],
+
+    skillsDescription: "",
+    skills: [
+        {
+            skillname: "HTML5"
+        },
+        {
+            skillname: "CSS"
+        },
+        {
+            skillname: "Reactjs"
+        }
+    ],
+
+    portfolio: [
+        {
+            name: "Look4Ward2It",
+            description: "Calendar to show Upcoming events.",
+            imgurl: "images/portfolio/phone.jpg"
+        },
+        {
+            name: "Social Underground",
+            description: "Blackjack",
+            imgurl: "images/portfolio/project.jpg"
+        },
+        {
+            name: "Social Underground 2: Electric Boog-a-loo",
+            description: "React-Blackjack",
+            imgurl: "images/portfolio/project2.png"
+        },
+    ],
+}
+export default resumeData
