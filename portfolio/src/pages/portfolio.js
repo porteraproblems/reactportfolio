95bd0bab23493b2d3e97b97e9dc1a3c5e028253e
Alexander Portera <aportera3@gmail.com>
2021-01-13 10:20:32 -0500

    commit

--- portfolio/src/pages/portfolio.js
+++ portfolio/src/pages/portfolio.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
 import Navi from "../Components/Nav/nav";
 
+function Portfolio () {
+    render (
+        <Container fluid className="container">
+            <Navi />
+            <Col>
+            </Col>
+        </Container>
+
+    )
+}
 
 
 
-
-export default Portfolio();
+export default Portfolio;
