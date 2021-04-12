7f3e28b04da079fa238176494fd36c183b7f3281
Alexander Portera <aportera3@gmail.com>
2021-04-12 16:36:49 -0400

    changed project 3

--- src/Components/Cards/cards.js
+++ src/Components/Cards/cards.js
@@ -3,6 +3,7 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import Card from 'react-bootstrap/Card';
 import SU2 from "../Images/SU2.png";
 import L42I from "../Images/L42I.png";
+import SU1 from "../Images/SU1.png";
 import "./cards.css";
 
 const ProjectCards = () => {
@@ -24,9 +25,9 @@ const ProjectCards = () => {
                 <Card className="bg-dark text-light">
                     <Card.Body className="body">
                         <Card.Img src={SU2} alt="Card Image" />
-                        <Card.Title tag="h5">Social Underground</Card.Title>
-                        <Card.Subtitle tag="h6" className="mb-2 text-muted">Second Group Project</Card.Subtitle>
-                        <Card.Text>This is a social hangout, a place where friends and family can go to play blackjack and RPS. There's a prototype of our version of the social deduction game "Werewolf".</Card.Text>
+                        <Card.Title tag="h5">Social Underground 2</Card.Title>
+                        <Card.Subtitle tag="h6" className="mb-2 text-muted">Third Group Project</Card.Subtitle>
+                        <Card.Text>This social hangout is all built in React. It is a place where friends and family can go to play blackjack and RPS. There's a prototype of our version of the social deduction game "Werewolf".</Card.Text>
                         <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://social-underground-2.herokuapp.com/signup">Take a look</Card.Link>
                         <br></br>
                         <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://github.com/rzelazny/social-underground-2">Check the Code</Card.Link>
@@ -35,12 +36,13 @@ const ProjectCards = () => {
                 <br></br>
                 <Card className="bg-dark text-light">
                     <Card.Body className="body">
-                        <Card.Title tag="h5">Quiz Time</Card.Title>
-                        <Card.Subtitle tag="h6" className="mb-2 text-muted">JavaScript Quiz</Card.Subtitle>
-                        <Card.Text>A quick five question quiz on Nintendo, how well do you know the company behind Super Mario?</Card.Text>
-                        <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://porteraproblems.github.io/QuizTime/">Take a look</Card.Link>
+                        <Card.Img src={SU1} alt="Card Image" />
+                        <Card.Title tag="h5">Social Underground</Card.Title>
+                        <Card.Subtitle tag="h6" className="mb-2 text-muted">Second Group Project</Card.Subtitle>
+                        <Card.Text>This is the first attempt a social hangout, a place where friends and family can go to play blackjack and RPS.</Card.Text>
+                        <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://social-underground-bc.herokuapp.com/">Take a look</Card.Link>
                         <br></br>
-                        <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://github.com/porteraproblems/QuizTime">Check the Code</Card.Link>
+                        <Card.Link className="cardLink" target="_blank" rel="noreferrer" href="https://github.com/rzelazny/social-underground">Check the Code</Card.Link>
                     </Card.Body>
                 </Card>
             </CardDeck>
